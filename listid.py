52df9ebc031431fcbe4e140db19d6d76590aebee
niko <niko.aoapp@gmail.com>
2025-01-15 14:12:05 +0200

    1

--- listid.py
+++ listid.py
@@ -1,2 +1,34 @@
-vokaali = ["a","e","u","o","i","ü","๕","๖","ไ"]
+from pickle import MARK
+import string
+
+vokaali = ["a","e","u","o","i","รผ","รต","รถ","รค"]
 konsonanti = "qwrtpsdfghjklzxcvbnm"
+markid = string.punctuation # %&/()=><{}[]
+v = k = m = t = 0
+
+while True:
+    tekst = input("Sisesta mingi tekst: ").lower()
+    
+    if tekst.isdigit():
+        break
+    else:
+        tekst_list = list(tekst)
+        print(tekst_list)
+
+        for taht in tekst_list:
+            if taht in vokaali:
+                v += 1
+            
+            elif taht in konsonanti:
+                k += 1
+            
+            elif taht in markid:
+                m += 1
+
+            elif taht == " ":
+                t += 1
+
+    print("Vokaali :", v)
+    print("Konsonanti :", k)
+    print("Markid :", m)
+    print("Tรผhikud :", t)
